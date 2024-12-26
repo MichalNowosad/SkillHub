20fe2269744547ea9c3d4053144abd5082b55a9a
Michal Nowosad <40039094+MichalNowosad@users.noreply.github.com>
2024-12-26 17:18:00 +0100

    Login page

--- SkillHub.Web/ClientApp/src/app/app.config.ts
+++ SkillHub.Web/ClientApp/src/app/app.config.ts
@@ -1,9 +1,15 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    // { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+  ]
 };
